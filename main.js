c269f012678aeedad839e319278b88b433f4853c
Nandhaprakash R <139529262+NandhaprakashR@users.noreply.github.com>
2024-01-30 09:38:16 +0530

    Add files via upload
    portfolio

---
+++ main.js
@@ -0,0 +1,5 @@
+$('.skill').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+}, {offset: '80%'});
