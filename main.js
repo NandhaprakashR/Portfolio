97ed6bee0313613a2fb69691ecb6f04801fc8741
Nandhaprakash R <139529262+NandhaprakashR@users.noreply.github.com>
2024-02-06 20:11:55 +0530

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-$('.skill').waypoint(function () {
-    $('.progress .progress-bar').each(function () {
-        $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-}, {offset: '80%'});
+
